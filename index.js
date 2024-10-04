3556a1371af1d1c4b1d9e87f448a8a866b150fa1
bhushan <bhushanpawara25@gmail.com>
2024-10-04 12:49:02 +0530

    origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://ecommerce-bhushan.vercel.app",
     credentials: true,
   })
 );
